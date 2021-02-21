917ec00277c967be3f8371ef6cb66153746c0104
dunow <dunow33@hotmail.com>
2021-02-21 00:13:07 -0500

    commit tree c68ae0532c6895dc22820a200ebd17267ad37901

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+function findMatching(drivers, string){
+    let driversArray = [];
+
+    drivers.filter(driver => {
+        if(driver.toLowerCase() === string.toLowerCase())
+            driversArray.push(driver);
+    });
+
+    return driversArray;
+}
+
+function fuzzyMatch(drivers, string){
+    let driversArray = [];
+
+    drivers.filter(driver => {
+        if(driver.slice(0, string.length) === string)
+            driversArray.push(driver);
+    });
+
+    return driversArray;
+}
+
+function matchName(drivers, string){
+    let driversArray = [];
+
+    drivers.filter(driver => {
+        if(driver.name === string)
+            driversArray.push(driver);
+    });
+
+    return driversArray;
+}
